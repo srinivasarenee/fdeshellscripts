9f8bf7d1623abed354926051b30da32ed78b17bf
srinivas <srinivas.renee@gmail.com>
2026-09-06 17:57:05 +0530

    updated new files

--- diskusage.sh
+++ diskusage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 disk=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
-if [ $disk -gt 80] ; then
+if [ $disk -gt 80 ] ; then
     echo "CRITICAL: Disk Usage $disk"
 else
     echo "NORMAL: Disk Usage $disk"
