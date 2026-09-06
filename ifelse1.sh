302991044bfa2e2e0875ec4d6df861ada74771c1
srinivas <srinivas.renee@gmail.com>
2026-09-06 16:59:08 +0530

    updated new files

--- ifelse1.sh
+++ ifelse1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 read -p "Enter a Number:"  num
 
-if[ $num -lt 10 ]; then
+if [ $num -lt 10 ]; then
     echo "$num is less than 10"
-elif[ $num -eq 10 ]; then
+elif [ $num -eq 10 ]; then
     echo "$num is eqaul to 10"
 else
     echo "$num is more then 10"
